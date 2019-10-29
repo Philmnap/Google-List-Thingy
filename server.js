1fa2b207112fdef2d152153e033e4a6737aef888
Philipp Rivadeneira <philmnap@gmail.com>
2019-10-29 18:33:43 -0500

    server stuff

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +12,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(MONGODB_URI);
+
+
+app.use(routes);
+
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
