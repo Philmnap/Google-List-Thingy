71f610062c3c89911af264df8f7fdca60d087f3f
Philipp Rivadeneira <philmnap@gmail.com>
2019-10-29 19:27:46 -0500

    more files

--- models/book.js
+++ models/book.js
@@ -1,15 +1,4 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+module.exports = {
+    Book: require("./book")
+};
 
-const bookSchema = new Schema({
-    _id: { type: String },
-    title: { type: String },
-    authors: { type: Array },
-    description: { type: String },
-    image: { type: String },
-    link: { type: String }
-});
-
-const Book = mongoose.model("Book", bookSchema);
-
-module.exports = Book;
